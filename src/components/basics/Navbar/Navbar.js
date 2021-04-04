c372ef8a60d762153031221b9ed59616d39e22a2
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-04-04 17:24:10 +0300

    add platforms changes

--- src/components/basics/Navbar/Navbar.js
+++ src/components/basics/Navbar/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, View} from "react-native";
+import {Platform, StyleSheet, View} from "react-native";
 import {THEME} from "../../../constants/theme";
 import {AppText} from "../../ui/AppText";
 
 
 export const Navbar = ({title}) => {
-    return <View style={styles.navbar}>
+    return <View style={{
+        ...styles.navbar,
+        ...Platform.select({
+            ios: styles.navbarIos,
+            android: styles.navbarAndroid
+        })
+    }}
+    >
         <AppText style={styles.text}>{title}</AppText>
     </View>
 };
@@ -13,13 +20,19 @@ export const Navbar = ({title}) => {
 const styles = StyleSheet.create({
     navbar: {
         height: 70,
-        backgroundColor: THEME.MAIN_COLOR,
         alignItems: 'center',
         justifyContent: 'flex-end',
-        paddingBottom:20
+        paddingBottom: 10
+    },
+    navbarAndroid: {
+        backgroundColor: THEME.MAIN_COLOR
+    },
+    navbarIos: {
+        borderBottomColor: THEME.MAIN_COLOR,
+        borderBottomWidth: 1
     },
     text: {
-        color: 'white',
+        color: Platform.OS === 'ios' ? THEME.MAIN_COLOR : '#fff',
         fontSize: 20
     }
 });
--- src/components/ui/AppButton.js
+++ src/components/ui/AppButton.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import {StyleSheet, View, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native';
 import { AppTextBold } from './AppTextBold';
 import { THEME } from '../../constants/theme';
 
 
 export const AppButton = ({ children, onPress, color = THEME.MAIN_COLOR }) => {
+  const Wrapper = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+
   return (
-    <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
+    <Wrapper onPress={onPress} activeOpacity={0.7}>
       <View style={{ ...styles.button, backgroundColor: color }}>
         <AppTextBold style={styles.text}>{children}</AppTextBold>
       </View>
-    </TouchableOpacity>
+    </Wrapper>
   );
 };
 
