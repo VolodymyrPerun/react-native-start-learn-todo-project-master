8b65fa142863bc8fe9069fabf3de5a2ae8744f82
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-04-04 17:58:09 +0300

    change device orientation parametres and adaptive

--- src/components/screens/MainScreen/MainScreen.js
+++ src/components/screens/MainScreen/MainScreen.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import {FlatList, Image, StyleSheet, View} from 'react-native';
-import {AddTodo} from "../../basics/AddTodo/AddTodo";
-import {Todo} from "../../basics/Todo/Todo";
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, Image, Dimensions } from 'react-native';
+import { AddTodo } from '../../basics/AddTodo/AddTodo';
+import { Todo } from '../../basics/Todo/Todo';
+import { THEME } from '../../../constants/theme';
 
 
-export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
+export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+    const [deviceWidth, setDeviceWidth] = useState(
+        Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+    );
+
+    useEffect(() => {
+        const update = () => {
+            const width =
+                Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+            setDeviceWidth(width);
+        }
+
+        Dimensions.addEventListener('change', update);
+
+        return () => {
+            Dimensions.removeEventListener('change', update);
+        };
+    });
+
     let content = (
-        <FlatList
-            keyExtractor={item => item.id.toString()}
-            data={todos}
-            renderItem={({item}) => (
-                <Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>
-            )}
-        />
+        <View style={{ width: deviceWidth }}>
+            <FlatList
+                keyExtractor={item => item.id.toString()}
+                data={todos}
+                renderItem={({ item }) => (
+                    <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+                )}
+            />
+        </View>
     );
 
     if (todos.length === 0) {
@@ -22,20 +43,13 @@ export const MainScreen = ({addTodo, todos, removeTodo, openTodo}) => {
                     style={styles.image}
                     source={require('../../../../assets/no-items.png')}
                 />
-                {/* <Image
-          style={styles.image}
-          source={{
-            uri:
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png'
-          }}
-        /> */}
             </View>
         );
     }
 
     return (
         <View>
-            <AddTodo onSubmit={addTodo}/>
+            <AddTodo onSubmit={addTodo} />
 
             {content}
         </View>
--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,5 +1,6 @@
 export const THEME = {
     DANGER_COLOR: '#e53935',
     GREY_COLOR: '#757575',
-    MAIN_COLOR: '#3949ad'
+    MAIN_COLOR: '#3949ad',
+    PADDING_HORIZONTAL: 30
 };
